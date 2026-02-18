218175dff6c066750df4f437b45d44ab95959961
TheUran <xb7995699@gmail.com>
2026-02-18 22:33:41 +0200

    Last commit into nothing also split version on minecraft version and modloader and a little changed version render in panel

--- engine/src/main/java/mchorse/bbs/gamemode/GameMode.java
+++ engine/src/main/java/mchorse/bbs/gamemode/GameMode.java
@@ -5,12 +5,14 @@ import mchorse.bbs.settings.values.ValueString;
 
 public class GameMode extends ValueGroup {
     public ValueString version = new ValueString("version", "");
+    public ValueString modLoader = new ValueString("modLoader", "");
     public ValueString description = new ValueString("description", "");
 
     public GameMode() {
         super("");
 
         this.add(this.version);
+        this.add(this.modLoader);
         this.add(this.description);
     }
 }
--- engine/src/main/java/mchorse/bbs/l10n/keys/IKey.java
+++ engine/src/main/java/mchorse/bbs/l10n/keys/IKey.java
@@ -5,13 +5,13 @@ import mchorse.bbs.BBS;
 import java.util.List;
 
 public interface IKey {
-    public static final IKey EMPTY = new StringKey("");
+    IKey EMPTY = new StringKey("");
 
-    public static IKey lang(String key) {
+    static IKey lang(String key) {
         return BBS.getL10n().getKey(key);
     }
 
-    public static IKey lang(String key, String content, IKey reference) {
+    static IKey lang(String key, String content, IKey reference) {
         LangKey langKey = BBS.getL10n().getKey(key, content);
 
         if (reference instanceof LangKey) {
@@ -24,17 +24,17 @@ public interface IKey {
     /**
      * This method is used to create an IKey that contains raw string data.
      */
-    public static IKey raw(String string) {
+    static IKey raw(String string) {
         return new StringKey(string);
     }
 
-    public static IKey comp(List<IKey> keys) {
+    static IKey comp(List<IKey> keys) {
         return new CompoundKey(keys);
     }
 
-    public String get();
+    String get();
 
-    public default IKey format(Object... args) {
+    default IKey format(Object... args) {
         return new FormatKey(this, args);
     }
 }
